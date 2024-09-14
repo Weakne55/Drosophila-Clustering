0b4ff735196c15d61b4f71c6c7615149aac9b9a8
Bogdan Karpovich <weakne55@node1.c.ibgene.org>
2024-09-14 11:01:13 +0000

    initial file

---
+++ Clusterization_karpovich.R
@@ -0,0 +1,4 @@
+ecoli_chip_path <- 'Dmel_immunity/ChIP/Ecoli_3_vs_Control_1_peak_compare_table.tsv'
+mlut_chip_path <- 'Dmel_immunity/ChIP/Mlut_5_vs_Control_1_peak_compare_table.tsv'
+ecoli_rna_path <- 'Dmel_immunity/RNA/Ecoli_XIvsControl_X/Ecoli_XIvsControl_X.mRNA.gene_level.deg.xls'
+mlut_rna_path <- 'Dmel_immunity/RNA/Mluteus_XIIvsControl_X/Mluteus_XIIvsControl_X.mRNA.gene_level.deg.xls'
